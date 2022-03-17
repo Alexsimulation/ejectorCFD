ed0440bc9e6e5a836754261155cba5896a80f1d5
Alexsimulation <alexis.angers@polymtl.ca>
2022-03-17 11:54:35 -0400

    Added area ratio

--- static2/batch.py
+++ static2/batch.py
@@ -2,12 +2,13 @@ from functions import *
 
 
 
-vars = ['ej_radi', 'ej_back_len', 'ej_z_delta']
+vars = ['ej_radi', 'ej_back_len', 'ej_z_delta', 'ej_area_ratio']
 
 values = [
     [0.6, 0.8, 1.0],
     [2, 3, 4],
     [0.15, 0.3, 0.5],
+    [1.44],
 ]
 
 run(vars, values)
